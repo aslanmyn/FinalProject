436a84f44b414c46e286b52b617d44fc4077a842
aslanmyn <mustafaev.aslan55@gmail.com>
2026-02-08 23:55:20 +0500

    fix errors

--- src/test/java/ru/kors/finalproject/FinalProjectApplicationTests.java
+++ src/test/java/ru/kors/finalproject/FinalProjectApplicationTests.java
@@ -2,9 +2,11 @@ package ru.kors.finalproject;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.test.context.ActiveProfiles;
 @SpringBootTest
+@ActiveProfiles("h2")
 class FinalProjectApplicationTests {
+
 
     @Test
     void contextLoads() {
